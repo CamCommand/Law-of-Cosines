73bafd009a6480fa98f7e9357d34417dfa33c6ef
CamCommand <48034712+CamCommand@users.noreply.github.com>
2020-10-05 14:15:03 -0400

    Add files via upload

---
+++ Law of Cosines/Law of Cosines/Form1.Designer.vb
@@ -0,0 +1,251 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frm1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblc2 = New System.Windows.Forms.Label()
+        Me.lbl1 = New System.Windows.Forms.Label()
+        Me.lbl2 = New System.Windows.Forms.Label()
+        Me.lbl3 = New System.Windows.Forms.Label()
+        Me.txta1 = New System.Windows.Forms.TextBox()
+        Me.txtb1 = New System.Windows.Forms.TextBox()
+        Me.txty1 = New System.Windows.Forms.TextBox()
+        Me.btn1 = New System.Windows.Forms.Button()
+        Me.lblc1 = New System.Windows.Forms.Label()
+        Me.lbly2 = New System.Windows.Forms.Label()
+        Me.btn2 = New System.Windows.Forms.Button()
+        Me.txtc1 = New System.Windows.Forms.TextBox()
+        Me.txtb2 = New System.Windows.Forms.TextBox()
+        Me.txta2 = New System.Windows.Forms.TextBox()
+        Me.lbl6 = New System.Windows.Forms.Label()
+        Me.lbl5 = New System.Windows.Forms.Label()
+        Me.lbl4 = New System.Windows.Forms.Label()
+        Me.lbly3 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'lblc2
+        '
+        Me.lblc2.AutoSize = True
+        Me.lblc2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblc2.Location = New System.Drawing.Point(81, 169)
+        Me.lblc2.Name = "lblc2"
+        Me.lblc2.Size = New System.Drawing.Size(30, 25)
+        Me.lblc2.TabIndex = 1
+        Me.lblc2.Text = "..."
+        '
+        'lbl1
+        '
+        Me.lbl1.AutoSize = True
+        Me.lbl1.Location = New System.Drawing.Point(28, 26)
+        Me.lbl1.Name = "lbl1"
+        Me.lbl1.Size = New System.Drawing.Size(25, 13)
+        Me.lbl1.TabIndex = 5
+        Me.lbl1.Text = "a = "
+        '
+        'lbl2
+        '
+        Me.lbl2.AutoSize = True
+        Me.lbl2.Location = New System.Drawing.Point(28, 56)
+        Me.lbl2.Name = "lbl2"
+        Me.lbl2.Size = New System.Drawing.Size(25, 13)
+        Me.lbl2.TabIndex = 6
+        Me.lbl2.Text = "b = "
+        '
+        'lbl3
+        '
+        Me.lbl3.AutoSize = True
+        Me.lbl3.Location = New System.Drawing.Point(28, 82)
+        Me.lbl3.Name = "lbl3"
+        Me.lbl3.Size = New System.Drawing.Size(24, 13)
+        Me.lbl3.TabIndex = 7
+        Me.lbl3.Text = "y = "
+        '
+        'txta1
+        '
+        Me.txta1.Location = New System.Drawing.Point(59, 23)
+        Me.txta1.Name = "txta1"
+        Me.txta1.Size = New System.Drawing.Size(100, 20)
+        Me.txta1.TabIndex = 11
+        '
+        'txtb1
+        '
+        Me.txtb1.Location = New System.Drawing.Point(59, 49)
+        Me.txtb1.Name = "txtb1"
+        Me.txtb1.Size = New System.Drawing.Size(100, 20)
+        Me.txtb1.TabIndex = 12
+        '
+        'txty1
+        '
+        Me.txty1.Location = New System.Drawing.Point(59, 75)
+        Me.txty1.Name = "txty1"
+        Me.txty1.Size = New System.Drawing.Size(100, 20)
+        Me.txty1.TabIndex = 13
+        '
+        'btn1
+        '
+        Me.btn1.Location = New System.Drawing.Point(31, 120)
+        Me.btn1.Name = "btn1"
+        Me.btn1.Size = New System.Drawing.Size(128, 23)
+        Me.btn1.TabIndex = 14
+        Me.btn1.Text = "Solve"
+        Me.btn1.UseVisualStyleBackColor = True
+        '
+        'lblc1
+        '
+        Me.lblc1.AutoSize = True
+        Me.lblc1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblc1.Location = New System.Drawing.Point(28, 169)
+        Me.lblc1.Name = "lblc1"
+        Me.lblc1.Size = New System.Drawing.Size(47, 25)
+        Me.lblc1.TabIndex = 17
+        Me.lblc1.Text = "c = "
+        '
+        'lbly2
+        '
+        Me.lbly2.AutoSize = True
+        Me.lbly2.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbly2.Location = New System.Drawing.Point(323, 169)
+        Me.lbly2.Name = "lbly2"
+        Me.lbly2.Size = New System.Drawing.Size(47, 25)
+        Me.lbly2.TabIndex = 26
+        Me.lbly2.Text = "y = "
+        '
+        'btn2
+        '
+        Me.btn2.Enabled = False
+        Me.btn2.Location = New System.Drawing.Point(326, 120)
+        Me.btn2.Name = "btn2"
+        Me.btn2.Size = New System.Drawing.Size(128, 23)
+        Me.btn2.TabIndex = 25
+        Me.btn2.Text = "Solve"
+        Me.btn2.UseVisualStyleBackColor = True
+        '
+        'txtc1
+        '
+        Me.txtc1.Enabled = False
+        Me.txtc1.Location = New System.Drawing.Point(354, 75)
+        Me.txtc1.Name = "txtc1"
+        Me.txtc1.Size = New System.Drawing.Size(100, 20)
+        Me.txtc1.TabIndex = 24
+        '
+        'txtb2
+        '
+        Me.txtb2.Enabled = False
+        Me.txtb2.Location = New System.Drawing.Point(354, 49)
+        Me.txtb2.Name = "txtb2"
+        Me.txtb2.Size = New System.Drawing.Size(100, 20)
+        Me.txtb2.TabIndex = 23
+        '
+        'txta2
+        '
+        Me.txta2.Enabled = False
+        Me.txta2.Location = New System.Drawing.Point(354, 23)
+        Me.txta2.Name = "txta2"
+        Me.txta2.Size = New System.Drawing.Size(100, 20)
+        Me.txta2.TabIndex = 22
+        '
+        'lbl6
+        '
+        Me.lbl6.AutoSize = True
+        Me.lbl6.Location = New System.Drawing.Point(323, 82)
+        Me.lbl6.Name = "lbl6"
+        Me.lbl6.Size = New System.Drawing.Size(25, 13)
+        Me.lbl6.TabIndex = 21
+        Me.lbl6.Text = "c = "
+        '
+        'lbl5
+        '
+        Me.lbl5.AutoSize = True
+        Me.lbl5.Location = New System.Drawing.Point(323, 56)
+        Me.lbl5.Name = "lbl5"
+        Me.lbl5.Size = New System.Drawing.Size(25, 13)
+        Me.lbl5.TabIndex = 20
+        Me.lbl5.Text = "b = "
+        '
+        'lbl4
+        '
+        Me.lbl4.AutoSize = True
+        Me.lbl4.Location = New System.Drawing.Point(323, 26)
+        Me.lbl4.Name = "lbl4"
+        Me.lbl4.Size = New System.Drawing.Size(25, 13)
+        Me.lbl4.TabIndex = 19
+        Me.lbl4.Text = "a = "
+        '
+        'lbly3
+        '
+        Me.lbly3.AutoSize = True
+        Me.lbly3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lbly3.Location = New System.Drawing.Point(376, 169)
+        Me.lbly3.Name = "lbly3"
+        Me.lbly3.Size = New System.Drawing.Size(30, 25)
+        Me.lbly3.TabIndex = 18
+        Me.lbly3.Text = "..."
+        '
+        'frm1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(593, 320)
+        Me.Controls.Add(Me.lbly2)
+        Me.Controls.Add(Me.btn2)
+        Me.Controls.Add(Me.txtc1)
+        Me.Controls.Add(Me.txtb2)
+        Me.Controls.Add(Me.txta2)
+        Me.Controls.Add(Me.lbl6)
+        Me.Controls.Add(Me.lbl5)
+        Me.Controls.Add(Me.lbl4)
+        Me.Controls.Add(Me.lbly3)
+        Me.Controls.Add(Me.lblc1)
+        Me.Controls.Add(Me.btn1)
+        Me.Controls.Add(Me.txty1)
+        Me.Controls.Add(Me.txtb1)
+        Me.Controls.Add(Me.txta1)
+        Me.Controls.Add(Me.lbl3)
+        Me.Controls.Add(Me.lbl2)
+        Me.Controls.Add(Me.lbl1)
+        Me.Controls.Add(Me.lblc2)
+        Me.Name = "frm1"
+        Me.Text = "Cosines"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents lblc2 As System.Windows.Forms.Label
+    Friend WithEvents lbl1 As System.Windows.Forms.Label
+    Friend WithEvents lbl2 As System.Windows.Forms.Label
+    Friend WithEvents lbl3 As System.Windows.Forms.Label
+    Friend WithEvents txta1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtb1 As System.Windows.Forms.TextBox
+    Friend WithEvents txty1 As System.Windows.Forms.TextBox
+    Friend WithEvents btn1 As System.Windows.Forms.Button
+    Friend WithEvents lblc1 As System.Windows.Forms.Label
+    Friend WithEvents lbly2 As System.Windows.Forms.Label
+    Friend WithEvents btn2 As System.Windows.Forms.Button
+    Friend WithEvents txtc1 As System.Windows.Forms.TextBox
+    Friend WithEvents txtb2 As System.Windows.Forms.TextBox
+    Friend WithEvents txta2 As System.Windows.Forms.TextBox
+    Friend WithEvents lbl6 As System.Windows.Forms.Label
+    Friend WithEvents lbl5 As System.Windows.Forms.Label
+    Friend WithEvents lbl4 As System.Windows.Forms.Label
+    Friend WithEvents lbly3 As System.Windows.Forms.Label
+
+End Class
---
+++ Law of Cosines/Law of Cosines/Form1.vb
@@ -0,0 +1,35 @@
+﻿Public Class frm1
+    Dim a1 As Integer
+    Dim b1 As Integer
+    Dim y1 As Double
+    Dim c11 As Integer
+    Dim c1 As Double
+    Dim a2 As Integer
+    Dim b2 As Integer
+    Dim c2 As Integer
+    Dim y2 As Double
+
+    Private Sub btn1_Click(sender As System.Object, e As System.EventArgs) Handles btn1.Click
+        a1 = txta1.Text
+        b1 = txtb1.Text
+        y1 = txty1.Text
+        y1 = y1 * Math.PI / 180
+        c1 = Math.Round(Math.Sqrt(Math.Pow(a1, 2) + Math.Pow(b1, 2) - 2 * a1 * b1 * Math.Cos(y1)), 2)
+
+        lblc2.Text = c1
+        txta2.Enabled = True
+        txtb2.Enabled = True
+        txtc1.Enabled = True
+        btn2.Enabled = True
+    End Sub
+
+    Private Sub btn2_Click(sender As System.Object, e As System.EventArgs) Handles btn2.Click
+        a2 = txta2.Text
+        b2 = txtb2.Text
+        c2 = txtc1.Text
+
+        y2 = Math.Acos((Math.Pow(a2, 2) + Math.Pow(b2, 2) - Math.Pow(c2, 2)) / (2 * a2 * b2))
+        y2 = Math.Ceiling(y2 * 180 / Math.PI)
+        lbly3.Text = y2
+    End Sub
+End Class
